b057fb678b315d5506167b3f747dca4a16e731ce
bestprogrammerkk <163173995+bestprogrammerkk@users.noreply.github.com>
2024-10-04 15:03:45 -0300

    kkkkkkkk

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'História',
+    'Qual foi o estopim para a segunda guerra mundial',
+    'A invasão da Alemanha na Polônia'
+
+)
+
+criaCartao (
+    'Filosofia',
+    'De quem é a frase "Quando você olha muito tempo para um abismo, o abismo olha para você"?',
+    'Friedrich Nietzche'
+)
+
+criaCartao(
+    'Raciocínio Lógico',
+    'Uma mãe tinha 30 reais e queria dividir entre suas duas filhas, que horas são?',
+    '15 para às 2'
+)
